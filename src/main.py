89cf6110c664b5a998f22d4f91ac9ccc3b2d10fb
Sachal Mohar <sachal16@uvic.ca>
2025-10-10 21:18:53 -0700

    data pipeline working & libraries configured

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import matplotlib
+import pandas as pd # CSV data
+import numpy as np # math / stats
+import matplotlib.pyplot as plt # visulize results
+import requests # to fetch live data
+
+
+data = pd.read_csv(r"C:\Users\Sachal Mohar\Documents\Summer 2025 Projects\football-streak-predictor\data\raw\E0.csv")
+print(data.head())
